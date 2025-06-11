5a33261f6b94fa28e158bc24f3b3f939684aa145
KENG ᅠ ᅠᅠ ᅠ <Keng7767@outlook.com>
2025-06-11 18:30:46 +0300

    Добавьте файлы проекта.

---
+++ PollSystemApp.Domain/Users/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PollSystemApp.Domain.Users;
+
+public class User: IdentityUser<Guid>
+{
+    public DateTime CreatedAt { get; set; }
+
+    public bool IasActive { get; set; }
+
+    
+}
