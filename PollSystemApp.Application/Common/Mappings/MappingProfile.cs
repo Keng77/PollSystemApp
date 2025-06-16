7822c4df8827154f8ef94163454872b401b6813e
Vladislau Kukharenko <kitbag2100@gmail.com>
2025-06-16 14:30:06 +0300

    feat(App): Add Mapping

---
+++ PollSystemApp.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using PollSystemApp.Application.Common.Dto.OptionDtos;
+using PollSystemApp.Application.Common.Dto.PollDtos;
+using PollSystemApp.Application.Common.Dto.PollResultDtos;
+using PollSystemApp.Application.Common.Dto.UserDtos;
+using PollSystemApp.Application.Common.Dto.VoteDtos;
+using PollSystemApp.Domain.Polls;
+using PollSystemApp.Domain.Users;
+using System.Linq;
+
+namespace PollSystemApp.Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+            CreateMap<OptionForCreationDto, Option>();
+            CreateMap<Option, OptionDto>();
+
+            
+            CreateMap<PollForCreationDto, Poll>()
+                .ForMember(dest => dest.Tags, opt => opt.Ignore());
+            CreateMap<PollForUpdateDto, Poll>();
+            
+            CreateMap<Poll, PollDto>()
+                .ForMember(dest => dest.Options, opt => opt.Ignore()) 
+                .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.Tags != null ? src.Tags.Select(t => t.Name).ToList() : new List<string>()));
+
+
+            CreateMap<Vote, VoteDto>();
+
+            
+            CreateMap<UserForRegistrationDto, User>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+
+
+            CreateMap<User, UserDto>();              
+                                                                                             
+
+            CreateMap<OptionVoteSummary, OptionVoteSummaryDto>()
+                .ForMember(dest => dest.OptionText, opt => opt.Ignore()); 
+
+            CreateMap<PollResult, PollResultDto>()
+                .ForMember(dest => dest.PollTitle, opt => opt.Ignore()) 
+                .ForMember(dest => dest.Options, opt => opt.MapFrom(src => src.Options));
+
+        }
+    }
+}
---
+++ PollSystemApp.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace PollSystemApp.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            
+
+            return services;
+        }
+    }
+}
