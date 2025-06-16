3df03943925b128638d0ff6f3d4cc1c452ecd157
Vladislau Kukharenko <kitbag2100@gmail.com>
2025-06-16 17:28:49 +0300

    feat(App): Improve repositories

---
+++ PollSystemApp.Application/Common/Interfaces/IPollResultRepository.cs
@@ -0,0 +1,15 @@
+﻿using PollSystemApp.Domain.Polls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PollSystemApp.Application.Common.Interfaces
+{
+    public interface IPollResultRepository : IRepositoryBase<PollResult>
+    {
+        Task<IEnumerable<PollResult>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
+        Task<PollResult?> GetByIdAsync(Guid id, bool trackChanges);
+    }
+}
