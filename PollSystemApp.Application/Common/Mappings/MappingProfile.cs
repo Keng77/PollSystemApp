4fa76cfb1ce4290d4dab8cd80c18fe0085b2420c
Vladislau Kukharenko <kitbag2100@gmail.com>
2025-06-29 14:43:21 +0300

    fix: Add missing automapper profile for CreatePollCommand

--- PollSystemApp.Application/Common/Mappings/MappingProfile.cs
+++ PollSystemApp.Application/Common/Mappings/MappingProfile.cs
@@ -4,6 +4,7 @@ using PollSystemApp.Application.Common.Dto.PollDtos;
 using PollSystemApp.Application.Common.Dto.PollResultDtos;
 using PollSystemApp.Application.Common.Dto.UserDtos;
 using PollSystemApp.Application.Common.Dto.VoteDtos;
+using PollSystemApp.Application.UseCases.Polls.Commands.CreatePoll;
 using PollSystemApp.Domain.Polls;
 using PollSystemApp.Domain.Users;
 
@@ -13,6 +14,9 @@ namespace PollSystemApp.Application.Common.Mappings
     {
         public MappingProfile()
         {
+
+            CreateMap<CreatePollCommand, Poll>()
+       .ForMember(dest => dest.Tags, opt => opt.Ignore());
 
             CreateMap<OptionForCreationDto, Option>();
             CreateMap<OptionForUpdateDto, Option>();
